99323a8c9fd1ccda6ee5226e74cbd578fbdca7e9
Antonio Ramos <ramos_antonio@live.com.pt>
2014-03-18 21:25:31 +0000

    Testint Intel APP Framework2

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-  'use strict';
-
-  var config = {
-    pkg: grunt.file.readJSON("package.json")
-  };
-
-  require("../phonegap/common").build(grunt, config);
-};
