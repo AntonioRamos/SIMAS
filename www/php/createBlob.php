e4c808c22fbce624ad2cbcd1830b5f92bca28643
Antonio Ramos <ramos_antonio@live.com.pt>
2014-06-10 20:24:31 +0100

    updated with navigation

--- www/php/createBlob.php
+++ www/php/createBlob.php
@@ -15,10 +15,13 @@
 
   $userID = mysql_real_escape_string($userID);
   $tableName2 = mysql_real_escape_string($tableName2);
-  $mime = 'audio/vnd.wav';
+  $mime = "audio/wav";
 
-  $sql = "INSERT INTO $tableName2 (mime,data) VALUES('$mime','$resultBlob')";
-  $result = mysql_query($sql);
+  $mime = stripslashes($mime);
+  $mime = mysql_real_escape_string($mime);
+
+  $sql = "INSERT INTO $tableName2 (userID,mime,data) VALUES('$userID','$mime','$resultBlob')";
+  $result = mysql_query($sql) or die("Couldn't add file to database");
 
   $sql2 = "UPDATE usertable SET tableName='$table' WHERE id = $userID";
   $result2 = mysql_query($sql2);
